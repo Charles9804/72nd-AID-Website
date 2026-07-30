7e6483532c00da946f7b81c8807558750f4cbf5d
Charles9804 <saddas1100005@gmail.com>
2026-07-31 01:19:52 +0800

    新增 Firebase 帳號認證與登入頁面

---
+++ pages/js/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPZufJ6lt7RcZoNvQNNgiQEEg8JE6ywZo",
+  authDomain: "nd-aid-website.firebaseapp.com",
+  projectId: "nd-aid-website",
+  storageBucket: "nd-aid-website.firebasestorage.app",
+  messagingSenderId: "182805578751",
+  appId: "1:182805578751:web:bdb941953840c597cc5dca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
